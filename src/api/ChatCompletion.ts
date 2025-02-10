7744a8bc1b19f1dda6862915a48bc344d70f4e2a
Johannes Sluis <joesluis51@gmail.com>
2025-02-10 09:37:40 -0800

    added prod backend link

--- src/api/ChatCompletion.ts
+++ src/api/ChatCompletion.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://jenni-manuscript-backend.onrender.com/',
+    // baseURL: 'https://jenni-manuscript-backend.onrender.com/',
+    // baseURL: 'http://localhost:3000', // for local testing
+    baseURL: 'https://jenni-manuscript-backend.fly.dev/', // for production
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
